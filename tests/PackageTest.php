73364a3c1f365fbe56fcf447c2be26336ff00404
Denis Smet <denis.smetannikov@yandex.ru>
2023-03-14 12:41:21 +0400

    New codestyle and PHP 8.1+

--- tests/PackageTest.php
+++ tests/PackageTest.php
@@ -1,28 +1,22 @@
 <?php
 
 /**
- * JBZoo Toolbox - Composer-Graph
+ * JBZoo Toolbox - Composer-Graph.
  *
  * This file is part of the JBZoo Toolbox project.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package    Composer-Graph
  * @license    MIT
  * @copyright  Copyright (C) JBZoo.com, All rights reserved.
- * @link       https://github.com/JBZoo/Composer-Graph
+ * @see        https://github.com/JBZoo/Composer-Graph
  */
 
 declare(strict_types=1);
 
 namespace JBZoo\PHPUnit;
 
-/**
- * Class ComposerGraphReadmeTest
- *
- * @package JBZoo\PHPUnit
- */
-class ComposerGraphReadmeTest extends AbstractReadmeTest
+final class PackageTest extends \JBZoo\Codestyle\PHPUnit\AbstractPackageTest
 {
     protected string $packageName = 'Composer-Graph';
 }
